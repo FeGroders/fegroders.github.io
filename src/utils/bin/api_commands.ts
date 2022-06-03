8c991254d7544fce06fc197378bdd62d8ea3ecf6
FeGroders <fernandogroder@gmail.com>
2022-06-03 08:29:26 -0300

    fix: changed the repo command

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -11,7 +11,7 @@ export const projects = async (args: string[]): Promise<string> => {
   return projects
     .map(
       (repo) =>
-        `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
+        `<u><a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.name}</a></u> - ${repo.description}`,
     )
     .join('\n');
 };
